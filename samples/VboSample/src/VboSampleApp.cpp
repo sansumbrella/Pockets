74ae6e2f1126e494213d36954bd27b481da4f511
David Wicks <david@sansumbrella.com>
2012-12-23 01:57:00 -0500

    Developing a rough outline of Vbo/Attribute classes.
    Learning more about the OpenGL setup in the process, thinking about
    how to arrange things to make interleaved data and multiple buffers
    easy to manage.

---
+++ samples/VboSample/src/VboSampleApp.cpp
@@ -0,0 +1,104 @@
+#include "cinder/app/AppCocoaTouch.h"
+#include "cinder/app/Renderer.h"
+#include "cinder/Surface.h"
+#include "cinder/gl/Texture.h"
+#include "cinder/Camera.h"
+
+#include "pockets/gl/Vbo.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+struct VertexData
+{
+//	float position;
+//  float normal;
+//  float color;
+//  float texcoord;
+  Vec3f	position;
+  Vec3f normal;
+  Color	color;
+  Vec2f texcoord;
+};
+
+class VboSampleApp : public AppCocoaTouch {
+  public:
+	virtual void	setup();
+	virtual void	resize( ResizeEvent event );
+	virtual void	update();
+	virtual void	draw();
+	virtual void	mouseDown( MouseEvent event );
+		
+	Matrix44f	mCubeRotation;
+	gl::Texture mTex;
+	CameraPersp	mCam;
+};
+
+void VboSampleApp::setup()
+{
+	mCubeRotation.setToIdentity();
+
+	// Create a blue-green gradient as an OpenGL texture
+	Surface8u surface( 256, 256, false );
+	Surface8u::Iter iter = surface.getIter();
+	while( iter.line() ) {
+		while( iter.pixel() ) {
+			iter.r() = 0;
+			iter.g() = iter.x();
+			iter.b() = iter.y();
+		}
+	}
+	
+	mTex = gl::Texture( surface );
+
+  pockets::gl::AttributeSet attributes;
+  attributes.addAttribute( "position", 3, GL_FLOAT, offsetof( VertexData, position ) );
+  attributes.addAttribute( "normal", 3, GL_FLOAT, offsetof( VertexData, normal ) );
+  attributes.addAttribute( "color", 3, GL_FLOAT, offsetof( VertexData, color ) );
+  attributes.addAttribute( "texcoord", 2, GL_FLOAT, offsetof( VertexData, texcoord ) );
+  attributes.setStride( sizeof( VertexData ) );
+
+  for( auto &attr : attributes )
+  {
+    cout << "Offset of: " << attr.getName() << " = " << attr.getOffset() << endl;
+  }
+
+  auto indices = pockets::gl::Vbo::create( pockets::gl::Attribute( "index", 1, GL_UNSIGNED_SHORT ) );
+  auto vertices = pockets::gl::Vbo::create( attributes );
+  auto mesh = pockets::gl::VboMesh::create();
+  mesh->addBuffer( indices );
+  mesh->addBuffer( vertices );
+}
+
+void VboSampleApp::resize( ResizeEvent event )
+{
+	mCam.lookAt( Vec3f( 3, 2, -3 ), Vec3f::zero() );
+	mCam.setPerspective( 60, event.getAspectRatio(), 1, 1000 );
+}
+
+void VboSampleApp::mouseDown( MouseEvent event )
+{
+	console() << "Mouse down @ " << event.getPos() << std::endl;
+}
+
+void VboSampleApp::update()
+{
+	mCubeRotation.rotate( Vec3f( 1, 1, 1 ), 0.03f );
+}
+
+void VboSampleApp::draw()
+{
+	gl::clear( Color( 0.2f, 0.2f, 0.3f ) );
+	gl::enable( GL_TEXTURE_2D );
+	gl::enableDepthRead();
+	
+	mTex.bind();
+	gl::setMatrices( mCam );
+	glPushMatrix();
+		gl::multModelView( mCubeRotation );
+		gl::drawCube( Vec3f::zero(), Vec3f( 2.0f, 2.0f, 2.0f ) );
+	glPopMatrix();
+}
+
+CINDER_APP_COCOA_TOUCH( VboSampleApp, RendererGl )
