9cff3b390b9b2e3f3b995c941e2a8e8d250549d7
David Wicks <david@sansumbrella.com>
2013-01-23 19:50:54 -0500

    Renamed uniquepath to nextnumberedpath.

--- src/pockets/FileUtils.cpp
+++ src/pockets/FileUtils.cpp
@@ -6,7 +6,7 @@
 using namespace ci;
 using namespace std;
 
-std::string pockets::getUniquePath( const fs::path &path, int padding, bool numberFirstFile )
+std::string pockets::getNextNumberedPath( const fs::path &path, int padding, bool numberFirstFile )
 {
 	fs::path p( path );
 	string extension = p.extension().string();
--- src/pockets/FileUtils.h
+++ src/pockets/FileUtils.h
@@ -7,8 +7,8 @@
 
 namespace pockets
 {
-//! Returns unique, numbered pathname based on the provided path in the form some/filename-##.ext
-std::string getUniquePath( const ci::fs::path &path, int padding = 2, bool numberFirstFile = true );
+//! Returns next numbered pathname based on the provided path in the form some/filename-##.ext
+std::string getNextNumberedPath( const ci::fs::path &path, int padding = 2, bool numberFirstFile = true );
 //! Returns a left-padded string based on the input string. e.g. ("two", 4, "0") returns "0two", ("four", 4, "0") returns "four"
 std::string leftPaddedString( const std::string &input, int minSize, const std::string pad="0" );
 
