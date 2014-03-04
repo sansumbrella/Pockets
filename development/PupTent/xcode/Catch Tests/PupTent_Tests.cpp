84c7e5d5c3ef98b7cf3f1f03d89df074019f1f27
David Wicks <wicks.db@gmail.com>
2014-03-04 11:10:50 -0500

    More unit tests.

--- development/PupTent/xcode/Catch Tests/PupTent_Tests.cpp
+++ development/PupTent/xcode/Catch Tests/PupTent_Tests.cpp
@@ -16,4 +16,6 @@ using namespace cinder;
 TEST_CASE( "Circuler Lerp", "[animation]" ) {
   REQUIRE( lerpWrapped( 0.1f, 1.0f, 1.0f, 0.5f ) == 1.05f );
   REQUIRE( lerpWrapped( 0.6f, 1.0f, 1.0f, 0.5f ) == 0.8f );
+  REQUIRE( lerpWrapped( 0.2f, 1.0f, 1.0f, 0.5f ) == 1.1f );
+  REQUIRE( lerpWrapped( 0.5f, 1.0f, 1.0f, 0.5f ) == 0.75f );
 }
