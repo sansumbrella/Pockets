3ac7d521cfd02c903b9ba31d810b2e6bcd99115c
David Wicks <wicks.db@gmail.com>
2014-04-28 08:42:18 -0400

    10k treasures.

--- development/PupTent/src/PupTentApp.cpp
+++ development/PupTent/src/PupTentApp.cpp
@@ -85,7 +85,7 @@ void PupTentApp::setup()
   mEntityX.systems.configure();
 
   createPlayer();
-  for( int i = 0; i < 1000; ++i )
+  for( int i = 0; i < 10000; ++i )
   {
     createTreasure();
   }
