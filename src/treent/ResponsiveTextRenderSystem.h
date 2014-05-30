4320bbd1f9da99fca829893e55e9defc598ebe06
bhavnaism <bhavna@bhavnaism.com>
2014-05-29 20:24:40 -0700

    working responsive text rendering, minus between-layout animation

--- src/treent/ResponsiveTextRenderSystem.h
+++ src/treent/ResponsiveTextRenderSystem.h
@@ -22,11 +22,26 @@ struct RespTextComponent : Component<RespTextComponent>
   //! Generates glyph placements for \a text. This is an optimization over storing the string.
   void setText( const std::string &text );
 
-  ci::TextLayout		_layout;
+  //! Reflows text within a TextLayout as value for that text changes
+  void reflowLayout( float val, const std::string &text );
+
+  //! Splits lines in a block of text based on a per-line character limit
+  void splitLines( const std::string &text, int charLimit, int lineNum );
+
+  //! Find longest word in the input string and use that to set max number of characters in a line
+  int setMaxChars( const std::string &text );
+
+  std::vector<ci::gl::TextureFont::DrawOptions>	_opts;	// vector of DrawOptions for each line of text
+  std::vector<GlyphPlacements>		_glyph_placements; // vector of GlyphPlacements for each line
+  float				_line_height;	// set height of each line of text
   ci::ColorA			_color;
+  ci::gl::TextureRef	_texture;
   ci::gl::TextureFontRef	_font;
-  float					_ranking;
-  GlyphPlacements		_glyph_placements;
+  float					_value;
+  float					_rect_width; // width of text box rectangle
+  float					_rect_height; // height of text box rectangle
+  float					_line_aspect_ratio; // ideal line aspect ratio
+  int					_max_chars;	// maximum number of characters in a line
 };
 
 class ResponsiveTextRenderSystem : public System<ResponsiveTextRenderSystem>
@@ -34,8 +49,12 @@ class ResponsiveTextRenderSystem : public System<ResponsiveTextRenderSystem>
 public:
   void update( EntityManagerRef entities, EventManagerRef events, double dt ) override;
   void draw() const;
+
+  float					_aspect_ratio = 0.519; // Default value for now
+
 private:
 	std::map<ci::gl::TextureFontRef, std::vector<std::pair<LocationComponentRef, RespTextComponentRef>>> _grouped_text;
+
 };
 
 } // treent::
